0cc944c5d0a514c9094ba9cb13767a819fac22a0
José Luis Romero <jose.romero@ing.uchile.cl>
2022-10-05 14:12:45 -0300

    Refactor typechecker

--- lib/typecheck.ml
+++ lib/typecheck.ml
@@ -3,6 +3,8 @@ open Env
 
 let ( let* ) = Option.bind
 
+type tenv = t env
+
 let typecheck_op1 (op : op1) (t : t) =
   match op with Not -> if t = BoolT then Some BoolT else None
 
@@ -11,19 +13,19 @@ let typecheck_op2 (op : op2) (t1 : t) (t2 : t) =
   | Plus | Minus -> if (t1, t2) = (NatT, NatT) then Some NatT else None
   | And | Or -> if (t1, t2) = (BoolT, BoolT) then Some BoolT else None
 
-let rec typecheck : t env -> expr -> t option =
- fun env expr ->
+let rec typecheck : tenv -> expr -> t option =
+ fun tenv expr ->
   match expr with
   | NatE _ -> Some NatT
   | BoolE _ -> Some BoolT
-  | VarE id -> Some (lookup id env)
+  | VarE id -> Some (lookup id tenv)
   | LetE (id, e1, e2) ->
-      let* t = typecheck env e1 in
-      typecheck (extend id t env) e2
+      let* t = typecheck tenv e1 in
+      typecheck (extend id t tenv) e2
   | Op1E (op, e) ->
-      let* t = typecheck env e in
+      let* t = typecheck tenv e in
       typecheck_op1 op t
   | Op2E (op, e1, e2) ->
-      let* t1 = typecheck env e1 in
-      let* t2 = typecheck env e2 in
+      let* t1 = typecheck tenv e1 in
+      let* t2 = typecheck tenv e2 in
       typecheck_op2 op t1 t2
